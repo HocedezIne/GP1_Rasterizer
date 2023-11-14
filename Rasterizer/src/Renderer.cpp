a3fa4d11aa09f0256bd064893359b4fea5e5234a
HocedezIne <ine.hocedez@student.howest.be>
2023-11-14 11:48:28 +0100

    todo 3 barycentric

--- Rasterizer/src/Renderer.cpp
+++ Rasterizer/src/Renderer.cpp
@@ -43,9 +43,9 @@ void Renderer::Render()
 	//Lock BackBuffer
 	SDL_LockSurface(m_pBackBuffer);
 
-	Render_W1_Part1(); //Rasterizer Stage Only
+	//Render_W1_Part1(); //Rasterizer Stage Only
 	//Render_W1_Part2(); //Projection Stage only
-	//Render_W1_Part3(); //Barycentric Coordinates
+	Render_W1_Part3(); //Barycentric Coordinates
 
 	//@END
 	//Update SDL Surface
@@ -58,7 +58,7 @@ void Renderer::VertexTransformationFunction(const std::vector<Vertex>& vertices_
 {
 	for (int i{}; i < /*vertices_in.size()*/3; i++)
 	{
-		vertices_out.push_back({ {m_Camera.viewMatrix.TransformPoint(vertices_in[i].position)} }); // transform with viewmatrix
+		vertices_out.push_back({ {m_Camera.viewMatrix.TransformPoint(vertices_in[i].position)}, vertices_in[i].color }); // transform with viewmatrix
 
 		// perspective divide
 		vertices_out[i].position.x /= vertices_out[i].position.z;
@@ -169,5 +169,55 @@ void Renderer::Render_W1_Part2()
 
 void Renderer::Render_W1_Part3()
 {
+	// Define Triangle - vertices in WORLD space
+	std::vector<Vertex> vertices_world
+	{
+		{ {0.f,4.f,2.f}, {1,0,0}},
+		{ {3.f,-2.f,2.f}, {0,1,0}},
+		{ {-3.f, -2.f,2.f}, {0,0,1}},
+	};
 
+	std::vector<Vertex> vertices_out{};
+	vertices_out.reserve(vertices_world.size());
+
+	VertexTransformationFunction(vertices_world, vertices_out);
+
+	for (int px{}; px < m_Width; ++px)
+	{
+		for (int py{}; py < m_Height; ++py)
+		{
+			ColorRGB finalColor{};
+
+			bool earlyReturn{ false };
+			std::vector<float> weights{};
+			weights.reserve(3);
+
+			for (int idx = 0; idx < 3; idx++)
+			{
+				Vector2 edge{ vertices_out[(idx + 1) % 3].position.x - vertices_out[idx].position.x,  vertices_out[(idx + 1) % 3].position.y - vertices_out[idx].position.y };
+				Vector2 vertexToPixel{ px - vertices_out[idx].position.x, py - vertices_out[idx].position.y };
+				weights.push_back(Vector2::Cross(edge, vertexToPixel));
+				if (weights[idx] < 0.f)
+				{
+					earlyReturn = true;
+					break;
+				}
+			}
+			if (!earlyReturn)
+			{
+				float triangleArea{ weights[0] + weights[1] + weights[2] };
+				finalColor = { vertices_out[0].color * (weights[1] / triangleArea) +
+							   vertices_out[1].color * (weights[2] / triangleArea) +
+							   vertices_out[2].color * (weights[0] / triangleArea) };
+			}
+
+			//Update Color in Buffer
+			finalColor.MaxToOne();
+
+			m_pBackBufferPixels[px + (py * m_Width)] = SDL_MapRGB(m_pBackBuffer->format,
+				static_cast<uint8_t>(finalColor.r * 255),
+				static_cast<uint8_t>(finalColor.g * 255),
+				static_cast<uint8_t>(finalColor.b * 255));
+		}
+	}
 }
